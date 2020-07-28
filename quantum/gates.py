8053e4d7ca20f7c917c4bef94a91faeb1c25fac8
Guen Prawiroatmodjo <guenp@microsoft.com>
2020-07-28 01:16:11 -0700

    add states, normalize gates

--- quantum/gates.py
+++ quantum/gates.py
@@ -1,13 +1,17 @@
 import numpy as np
+from quantum.states import zero, one
 
 def _gate(*values):
     return np.array(values)
 
+def _norm(gate):
+    return gate / np.linalg.norm(gate, axis = 1, keepdims = True)
+
 I = _gate((1, 0), (0, 1))
-H = _gate((1,1), (1,-1))
+H = _norm(_gate((1,1), (1,-1)))
 X = _gate((0, 1), (1, 0))
 Y = _gate((0, 1j), (1j, 0))
 Z = _gate((1, 0), (0, -1))
-P0 = np.kron(_gate(1, 0), _gate(0, 1).T)
-P1 = np.kron(_gate(0, 1), _gate(1, 0).T)
+P0 = np.kron(zero, zero.T)
+P1 = np.kron(one, one.T)
 CNOT01 = np.kron(P0, I) + np.kron(P1, X)
--- quantum/states.py
+++ quantum/states.py
@@ -1,4 +1,12 @@
 import numpy as np
 
 def _state(*values):
-    return np.array([values])
+    return np.array([values]).T
+
+def _norm(state):
+    return state / np.linalg.norm(state)
+
+zero = _state(1,0)
+one = _state(0,1)
+plus = _norm(_state(1,1))
+minus = _norm(_state(1,-1))
