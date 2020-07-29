86b0fdd628d96c5ade8f55ba26c550b9bfd9748a
Guen Prawiroatmodjo <guenp@microsoft.com>
2020-07-29 16:19:40 -0700

    fix bug in gates UT

--- quantum/test/test_gates.py
+++ quantum/test/test_gates.py
@@ -2,4 +2,4 @@ import numpy as np
 from quantum.gates import H
 
 def test_gates():
-    assert (H == np.array([[1, 1], [1, -1]])).all()
+    assert (H == np.array([[1, 1], [1, -1]])/np.sqrt(2)).all()
