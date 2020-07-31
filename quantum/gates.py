8a69db26299f77a0c6bba4b548be5879b7667738
Guen Prawiroatmodjo <guenp@microsoft.com>
2020-07-31 09:57:23 -0700

    add T, CNOT12, CNOT21

--- quantum/gates.py
+++ quantum/gates.py
@@ -17,6 +17,12 @@ P0 = np.kron(zero, zero.T)
 P1 = np.kron(one, one.T)
 CNOT01 = np.kron(P0, I) + np.kron(P1, X)
 CNOT10 = np.kron(I, P0) + np.kron(X, P1)
+CNOT12 = np.kron(I, CNOT01)
+CNOT21 = np.kron(I, CNOT10)
+CNOT012 = np.kron(CNOT01,I)
+CNOT102 = np.kron(CNOT10,I)
+T = np.array([[1, 0], [0, np.exp(1j*np.pi/4)]])
+TD = np.conjugate(T)
 
 name_gates = {
     "I": I,
@@ -24,8 +30,14 @@ name_gates = {
     "X": X,
     "Y": Y,
     "Z": Z,
+    "T": T,
+    "TD": TD,
     "CNOT01": CNOT01,
     "CNOT10": CNOT10,
+    "CNOT012": CNOT012,
+    "CNOT102": CNOT102,
+    "CNOT12": CNOT12,
+    "CNOT21": CNOT21,
 }
 
 def add_gate(label: str, value: np.ndarray, overwrite: bool = False):
