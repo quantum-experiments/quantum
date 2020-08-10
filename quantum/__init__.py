c3307df5987ae65ea422a251969bf1bbc56d1418
Guen Prawiroatmodjo <guenp@microsoft.com>
2020-08-09 18:41:28 -0700

    put ipython import in try/except block

--- quantum/__init__.py
+++ quantum/__init__.py
@@ -1,4 +1,8 @@
-from IPython.core.getipython import get_ipython
+try:
+    from IPython.core.getipython import get_ipython
+except:
+    def get_ipython():
+        return False
 
 if get_ipython():
     from quantum.magic import quantum
